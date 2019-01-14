8e8a7faf7d61aebd6e0ad4ebdb8c5c0eab355167
Quansheng.Xiong <Quansheng.Xiong@fdmgroup.com>
2019-01-14 09:58:35 -0500

    added postinstall

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/fridaygame'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/fridaygame/index.html'));
+  const index = path.join(__dirname, 'dist', 'index.html');
+  res.sendFile(index);
 });
 // Start the app by listening on the default Heroku port
 console.log("Server Started");
